207f3bf56982468c467139fa0ce062efb7430d3f
DanillGodovan <bonkinans@gmail.com>
2018-10-23 21:19:23 +0300

    Why the ArrayManipulation retrurns a prototype obj

---
+++ Homework3.js
@@ -0,0 +1,45 @@
+//1) подзадание
+
+let arr = [2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 0]
+ function arrayManipulation (array) {
+	let min = max = array.find(function (item) {
+		if (isFinite(item) && +item === item) {
+			return item
+		}
+	})
+	let sum = 0
+	array = array.filter(function (item) {
+    if (typeof item === 'number') {
+			if (item < min) {
+				min = item
+			}
+			if (item > max) {
+				max = item
+			}
+			sum += item
+		}
+		return item !== min && item !== max
+	})
+
+	 return {
+	 	min,
+	 	max,
+	 	sum,
+	 	array,
+
+  }
+  callback(min, max, sum )
+  }
+
+arrayManipulation(arr) // why it return prototype object?
+
+
+//2) подзадание
+
+
+
+
+arrayManipulation(function (max, min, sum) {
+    // эта анонимная функция выполнится после вызова callback-функции
+    console.log("callback called! " + arrayManipulation);
+});
