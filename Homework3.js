94fca039ef1d822c43dd17df87fc494b1b12bb80
DanillGodovan <bonkinans@gmail.com>
2018-10-27 00:02:36 +0300

    Третье задание выполнено, но не до конца

--- Homework3.js
+++ Homework3.js
@@ -28,11 +28,18 @@ let arr = [2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 0]
 	 	array,
 
   }
-  callback(min, max, sum )
+
+   let arrInObj = new Object(min, sum, array);
+   arrInObj["array"] = this.array.array;
+   arrInObj["sum"] = this.array.sum;
+   arrInObj["min"] = this.array.min;
+   arrInObj["max"] = this.array.max;
+   console.log(arrInObj)
   }
 
-arrayManipulation(arr) // why it return prototype object?
+arrayManipulation(arr)
 
+ alert(typeof arr);
 
 //2) подзадание
 
@@ -41,5 +48,34 @@ arrayManipulation(arr) // why it return prototype object?
 
 arrayManipulation(function (max, min, sum) {
     // эта анонимная функция выполнится после вызова callback-функции
-    console.log("callback called! " + arrayManipulation); // but it has write in console the array.find not a function?
+    console.log("callback called! " + arrayManipulation(arr)); // Но оно пишет что array.find not a function
 });
+
+
+
+// 3)
+
+ let arr2 = [1, 5, 3, 8, 5, 4, 2, 5];
+   function fromArrToObj() {
+
+    let obj2 = new Object(arr2);
+
+
+
+ obj2["1"] = arr2.forEach(function(item, i, arr) {
+console.log( i + ": " + item + " (массив:" + arr2 + ")" );
+});
+
+    let obj3 = new Object();
+    let i =  arr2.filter(function(number) {
+    return number > 0;
+    });
+    obj3["2"] = obj2
+console.log(Object.keys(obj3));
+
+    return {
+      obj2,
+      obj3
+    }
+       }
+   fromArrToObj(arr2)
