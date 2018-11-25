7462a8da2c2085827ac7a1034e291ad903fe0018
DanillGodovan <bonkinans@gmail.com>
2018-11-25 15:16:18 +0200

    Homework6 have added

--- homework4.js
+++ homework4.js
@@ -37,16 +37,13 @@ console.log(squareRoot); // [1, 2 ,3]
 
   // 3) mySort
 
-  function msort(arr){
-    for(var i = 0;i < arr.length; i++){
-        for(var j = i + 1; j < arr.length; j++){
-            if( arr[i] > arr[j] ){
-                var swap = arr[i];
-                arr[i] = arr[j];
-                arr[j] = swap;
-            }
-        }
-    }
-return arr;
-}     // сортировка по порядку
- arr.apply(msort())
+       // сортировка по порядку
+ arr.forEach(function (a, b) {
+   if (a > b) {
+     return 1
+   }
+  if (a <= b) {
+    return -1
+  }
+});
+console.log(arr)
