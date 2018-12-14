af9848f015817bb37c1d1048e97c029a3570dabf
DaniilGodovan <bonkinan@gmail.com>
2018-12-14 18:21:43 +0200

    homework 7 have added, homework8 doesn't work

--- html/homework8/getinform.js
+++ html/homework8/getinform.js
@@ -3,15 +3,9 @@ function promiseFunction (url) {
   fetch(url)
     .then(function (response) {
     	response.json().then(function(data) {
-            starwars.innerHTML = data ;
+          starwars.innerHTML =  data;
         });
   	})
-    function(response) {
-      if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-          response.status);
-        return;
-      }
     .catch(function (error) {
     	alert('oops');
   	})
@@ -23,11 +17,11 @@ async function asyncFunction (url) {
   try {
     let response = await fetch(url);
     let data = await response.json();
-    starwars.innerHTML = data ;
+  starwars.innerHTML = data;
   } catch (error) {
     alert('oops');
   }
 }
 
 promiseFunction('https://swapi.co/api/planets/1/')
-asyncFunction('https://swapi.co/api/planets/1/')
+asyncFunction('https://swapi.co/api/planets/1/') // don't work
