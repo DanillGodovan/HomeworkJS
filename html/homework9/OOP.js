729a3b59742becdd099d34fa651587161b9cbe88
DaniilGodovan <bonkinan@gmail.com>
2018-12-28 16:09:00 +0200

    errors has been fixed

--- html/homework9/OOP.js
+++ html/homework9/OOP.js
@@ -13,44 +13,57 @@ function Machine(power) {
   };
 }
 
-
-
-
 function Factory(power) {
-   Machine.apply(this, arguments);
-   let amountCloneMachine = 0;
-
-   this.addCloneMachine = function() {
-      if (this._enable === false) {
-        console.log('Clone Machine is turned off');
-      } else if (cloneMachine + 1   > power / 100) {
-      console.log ('To create not enough energy')
-    } else if (this._enable === true) {
-      	cloneMachine + 1
-     }
-   }
-
-   this.getCloneMachines = function() {
-    let dup_CloneMachine = cloneMachine.slice();
-    return dup_CloneMachine;
-   }
- }
-
-function CloneMachines(power) {
+  // унаследовать
   Machine.apply(this, arguments);
+
+  this.machine = []; // приватное свойство machine
+
+  this.addMachine = function() {
+    if (!this._enabled) {
+      throw new Error("Холодильник выключен");
+    }
+    if (this.machine.length + arguments.length >= this._power / 100) {
+      throw new Error("Нельзя добавить, не хватает мощности");
+    }
+    for (var i = 0; i < arguments.length; i++) {
+      this.machine.push(arguments[i]); // добавить всё из arguments
+    }
+
+  };
+
+  this.getMachine = function() {
+    // копируем еду в новый массив, чтобы манипуляции с ним не меняли food
+    return this.machine.slice();
+  };
+
+  this.filterMachine = function(filter) {
+    return this.machine.filter(filter);
+  };
+
+  this.removeMachine = function(item) {
+    var idx = this.machine.indexOf(item);
+    if (idx != -1) this.machine.splice(idx, 1);
+  };
+}
+
+
+
+
+function CloneMachine(power, machine) {
   Factory.apply(this, arguments);
   let humans = [];
 
   this.addHuman = function(word) {
-    if (this.amountCloneMachine < 1) {
-      console.log("You don't have no one clone machine")
+    if (this._enable === false) {
+      console.log('Clone Machine is turned off');
     } else if (humans.length + 1 > power / 100) {
       console.log("Too much humans!")
-    } else if (this.amountCloneMachine >= 1) {
+    } else if (this.machine.length >= 1) {
       humans.push(word)
     }
 }
-    this.getCloneMachines = function() {
+    this.getHumans = function() {
      let dup_Humans = humans.slice();
      return dup_Humans;
     }
@@ -62,4 +75,23 @@ function CloneMachines(power) {
 
 
 
-var fridge = new Factory(500);
+  var fridge = new CloneMachine(500);
+  fridge.enable();
+  fridge.addMachine({
+    title: "merby"
+  });
+  fridge.addMachine({
+    title: "morin"
+  });
+  fridge.addMachine({
+    title: "rin"
+  });
+  fridge.addMachine({
+    title: "alin"
+  });
+
+
+  fridge.removeMachine("нет таких машин"); // без эффекта
+  alert( fridge.getMachine().length ); // 4
+  fridge.addHuman("Jake");
+  fridge.getHumans()
